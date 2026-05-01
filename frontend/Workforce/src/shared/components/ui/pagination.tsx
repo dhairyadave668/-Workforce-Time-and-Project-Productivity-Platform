a0640cc01e89b5b67c3979d2a5cb4c23cd1b52af
dhairyadave668 <22it25@svitvasad.ac.in>
2026-05-01 16:41:55 +0530

    fixed shadcn files in ui shared folder

--- frontend/Workforce/src/shared/components/ui/pagination.tsx
+++ frontend/Workforce/src/shared/components/ui/pagination.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react";
 
 import { buttonVariants } from "@/shared/components/ui/button"; // Correct import path
